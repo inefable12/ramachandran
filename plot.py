b1e36cea4a120015018b1a448c1a3f2178c3ce38
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-14 01:49:47 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,10 +5,16 @@ from ramachandraw.parser import get_phi_psi
 from ramachandraw.utils import fetch_pdb 
 from ramachandraw.utils import plot
 
-st.title("Ramachandran Plot Generator")
+st.title("Generador del Diagrama de Ramachandran")
 
 pdb_id = st.text_input("Escribe el código de 4 dígitos: ", "3PL1")
 # Draw the Ramachandran plot
-plot(fetch_pdb(pdb_id))
+pdb_file = fetch_pdb(pdb_id))
 # Generate a dictionary to store the (phi, psi) torsion angles
-torsion_angles = get_phi_psi(fetch_pdb(pdb_id))
+#torsion_angles = get_phi_psi(fetch_pdb(pdb_id))
+
+plt.figure()
+    plot(pdb_file)
+
+st.markdown("### Ramachandran Plot of given Protein: ")
+    st.pyplot()
