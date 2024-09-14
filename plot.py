ac122611ad1995f1c1d37fbce1637899ed77d716
EHN8829 <enhonda1@gmail.com>
2024-09-14 09:33:01 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ from ramachandraw.utils import fetch_pdb
 from ramachandraw.utils import plot
 
 st.title("Generador de Diagrama de Ramachandran")
-st.text("Autor: Jesus Alvarado")
+st.text("Autor: EHN")
 
 pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "3PL1")
 pdb_file = fetch_pdb(pdb_id)
