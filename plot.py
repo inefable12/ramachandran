c05c5095c3b53d1595f1e37fb6efeb1ce33db972
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-14 01:38:06 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,13 +1,13 @@
 import streamlit as st
 #from RamachanDraw import fetch, phi_psi, plot
 import matplotlib.pyplot as plt
-from ramachandraw.parser import get_phi_psi as phi_psi
-from ramachandraw.utils import fetch_pdb as fetch
+from ramachandraw.parser import get_phi_psi
+from ramachandraw.utils import fetch_pdb 
 from ramachandraw.utils import plot
 
-"""
+st.title("Ramachandran Plot Generator")
 # PDB id
-pdb_id = "1mbn"
+pdb_id = st.text_input("Escribe el código de 4 dígitos: ", "3PL1")
 # Draw the Ramachandran plot
 plot(fetch_pdb(pdb_id))
 # Generate a dictionary to store the (phi, psi) torsion angles
