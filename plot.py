58988b477c37553aa13efc1d530d2a0bf056f593
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-16 01:25:16 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,7 +7,8 @@ from io import BytesIO
 
 st.title("Generador de Diagrama de Ramachandran")
 st.text("Autor: Jesús Alvarado-Huayhuaz")
-st.image("ramachandran_logo.png", caption="Jesus Alvarado-Huayhuaz")
+
+st.sidebar.image("ramachandran_logo.png", caption="Jesus Alvarado-Huayhuaz")
 
 pdb_id = st.text_input("Escribe el código PDB de 4 dígitos, por ejemplo: ", "3PL1")
 pdb_file = fetch_pdb(pdb_id)
